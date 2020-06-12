432b1fef90ca60792a32290d89a2ed230456c5e6
longmt <longmt2111@gmail.com>
2020-06-12 11:19:02 +0700

    add- C12.1 - RQ12 Màn hình chính app

--- Code/TimtroApp/Screen/HomeScreen.js
+++ Code/TimtroApp/Screen/HomeScreen.js
@@ -1,15 +1,79 @@
 import React,{useEffect,useState} from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Button, Image, Alert } from 'react-native';
-import { ScrollView, TextInput } from 'react-native-gesture-handler';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default function HomeScreen(navigation ) {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.TitleText}>HOME MENU</Text>
+      <View style={styles.nav}>
+      <Text>LOGO</Text>
+      </View>
+    
+      <View style={styles.body}>
+        <View style={styles.slideshow}>
+            <Image style={{height:138,width:'100%', borderRadius: 15,}} source={require('../img/slide1.jpg')}/>
+        </View>
+        <View>
+          <View style={{width:'100%',marginTop: 10, marginBottom: 20}}>
+            <Text style={styles.Title}>Tin đăng</Text>
+            <View style={{flexDirection: 'row',}}>
+              <TouchableOpacity style={{alignItems: 'center',}}
+              onPress={()=>{
 
- 
+              }}>
+              <Image style={styles.imageItem} source={require('../img/tim_tro.png')}/>
+              <Text>Tìm trọ</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={{alignItems: 'center',}}
+              onPress={()=>{
+                
+              }}>
+              <Image style={styles.imageItem} source={require('../img/ghep.png')}/>
+              <Text>Ở ghép</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={{width:'100%',marginTop: 10,}}>
+            <Text style={styles.Title}>Dịch vụ</Text>
+            <View style={{flexDirection: 'row',}}>
+              <TouchableOpacity style={{alignItems: 'center',}}
+              onPress={()=>{
+                
+              }}>
+              <Image style={styles.imageItem} source={require('../img/logistic.png')}/>
+              <Text>Vẩn chuyển</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={{alignItems: 'center',}}
+              onPress={()=>{
+                
+              }}>
+              <Image style={styles.imageItem} source={require('../img/logistic.png')}/>
+              <Text>Unknow</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </View>
 
+      <View style={styles.controler}>
+        <TouchableOpacity onPress={()=>{
+          alert('Trang chủ');
+        }}>
+      <Image style={styles.imgControler} source={require('../img/trangChu.png')}/>
+       </TouchableOpacity>
+
+       <TouchableOpacity onPress={()=>{
+          alert('Thêm tin');
+        }}>
+      <Image style={styles.imgControler} source={require('../img/add.png')}/>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={()=>{
+          alert('Menu');
+        }}>
+      <Image style={styles.imgControler} source={require('../img/tuyChonMenu.png')}/>
+      </TouchableOpacity>
+      </View>
+      
     </View>
   );
 }
@@ -17,52 +81,53 @@ export default function HomeScreen(navigation ) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFF',
+    backgroundColor: '#fff',
     alignItems: 'center',
-  },
-  TitleText: {
-    marginTop: 40,
-    marginBottom: 100,
-    fontSize: 40,
-    fontWeight: "bold"
-  },
-
-  Title: {
-    margin: 5,
-    padding: 5,
-    fontSize: 20,
-    backgroundColor: "#fff",
-    fontWeight: "bold"
-  },
-  UseText: {
-    height: 50,
-    width: 320,
-    borderWidth: 1,
-    color: '#000',
-    padding: 10
-  },
-  ButtonLogin: {
-    marginTop: 40,
-    height: 50,
-    width: 150,
    
-    backgroundColor: 'red'
   },
-
-  PassText: {
-    height: 50,
-    width: 320,
+  nav: {
+    width: '100%',
+    height: '8.5%',
+    backgroundColor: '#f3febd',
+   
+  },
+  body: {
+    width: '100%', height: '83.5%',
+    backgroundColor: '#f3febd',
+    alignItems: 'center',
+   
+  },
+  controler: {
+    height: '8%', width: '100%',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    
+  },
+  imgControler: {
+    marginTop: 10,
+    marginRight: 40, marginLeft: 40,
+    height: 30,
+    width: 30,
+   
+  },
+  slideshow: {
+    marginTop: 8,
+    height: 140,
+    backgroundColor: '#fff',
+    width: '95%',
     borderWidth: 1,
-    color: '#000',
-    marginTop: 20,
-    padding: 10
+    borderColor: 'black',
+    borderRadius: 15,
+   
   },
-  TextButtonLogin: {
-    height: 50,
-    width: 150,
-    textAlign: "center",
-    lineHeight: 50,
-    fontSize: 16,
-    color: '#fff'
+  imageItem: {
+    width: 80,
+    height: 80,
+    marginRight: 35,
+    marginLeft: 35,
   },
+  Title: {
+    fontSize: 20, 
+    marginBottom: 10,
+  }
 });
